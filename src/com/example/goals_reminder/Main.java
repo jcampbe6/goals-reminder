5668690f5ae11ef0854c85e2bfff73ecf6840546
jcampbe6 <jcampbe6@ggc.edu>
2014-03-07 04:54:59 -0500

    recovery of lost data

--- src/com/example/goals_reminder/Main.java
+++ src/com/example/goals_reminder/Main.java
@@ -1,26 +1,68 @@
 package com.example.goals_reminder;
 
+import android.app.ActionBar;
+import android.app.ActionBar.Tab;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
-import android.app.Activity;
-import android.view.Menu;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.ViewPager;
 
-public class Main extends Activity
+public class Main extends FragmentActivity implements ActionBar.TabListener
 {
+	private ViewPager viewPager;
+	private TabsPagerAdapter mAdapter;
+	private ActionBar actionBar;
+	private String[] tabs = { "Resolutions / Goals", "Reminders" };
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main_activity);
+		
+		// Initialization
+		viewPager = (ViewPager) findViewById(R.id.pager);
+		actionBar = getActionBar();
+		mAdapter = new TabsPagerAdapter(getSupportFragmentManager());
+		
+		viewPager.setAdapter(mAdapter);
+		actionBar.setHomeButtonEnabled(false);
+		actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+		
+		// Adding Tabs
+		for (String tab_name : tabs)
+		{
+			actionBar.addTab(actionBar.newTab().setText(tab_name).setTabListener(this));
+		}
+		
+		/**
+		 * on swiping the viewpager make respective tab selected
+		 */
+		viewPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+			
+			@Override
+			public void onPageSelected(int position)
+			{
+				actionBar.setSelectedNavigationItem(position);
+			}
+
+			public void onPageScrollStateChanged(int arg0){}
+					
+			public void onPageScrolled(int arg0, float arg1, int arg2){}	
+		});
 	}
 	
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu)
+	public void onTabReselected(ActionBar.Tab paramTab, FragmentTransaction paramFragmentTransaction)
 	{
-		// Inflate the menu; this adds items to the action bar if it
-		// is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
 	}
 	
+	public void onTabSelected(Tab tab, FragmentTransaction ft)
+	{
+		// show respected fragment view when tab selected
+		viewPager.setCurrentItem(tab.getPosition());
+	}
+	
+	public void onTabUnselected(ActionBar.Tab paramTab, FragmentTransaction paramFragmentTransaction)
+	{
+	}
 }
